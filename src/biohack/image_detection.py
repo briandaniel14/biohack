6ae3417f38354b3bbc2823bddeb6cc250f655216
briandaniel14 <briandaniel789@gmail.com>
2026-03-20 17:48:06 +0000

    Add detect filaments description

--- src/biohack/image_detection.py
+++ src/biohack/image_detection.py
@@ -1,2 +1,10 @@
 def detect_filaments(image):
+    """
+    Objective: Reliably isolate
+    filamentous structures from
+    background noise and nonfilamentous spots.
+    Challenge: Must handle varying
+    signal-to-noise ratios in
+    fluorescence data.
+    """
     pass
