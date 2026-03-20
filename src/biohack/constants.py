d464a34050dce91e00a9f66abea5c44bd71fb0cf
briandaniel14 <briandaniel789@gmail.com>
2026-03-20 18:21:11 +0000

    Add constants

---
+++ src/biohack/constants.py
@@ -0,0 +1,11 @@
+TIFF_PATHS = (
+    "data/ch20_URA7_URA8_001-crop1.tiff",
+    "data/ch20_URA7_URA8_001-crop2.tiff",
+    "data/ch20_URA7_URA8_001-crop3.tiff",
+    "data/ch20_URA7_URA8_002-crop1.tiff",
+    "data/ch20_URA7_URA8_002-crop2.tiff",
+    "data/ch20_URA7_URA8_002-crop3.tiff",
+    "data/ch20_URA7_URA8_002-crop4.tiff",
+    "data/ch20_URA7_URA8_002-crop5.tiff",
+    "data/ch20_URA7_URA8_002-crop6.tiff",
+)
