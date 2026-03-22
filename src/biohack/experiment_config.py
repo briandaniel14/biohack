de856c1915be7f06c2587a3dcdaceb42f22dafa0
Raxo77 <games@rott.me>
2026-03-22 10:12:07 +0000

    fix indent

--- src/biohack/experiment_config.py
+++ src/biohack/experiment_config.py
@@ -108,9 +108,9 @@ class ExperimentConfig:
                 float(x) for x in cast(list[Any], self.frangi_sigmas)
             )
 
-@classmethod
-def from_dict(cls, mapping: Mapping[str, Any]) -> ExperimentConfig:
-    """Build :class:`ExperimentConfig` from a YAML/dict, ignoring unknown keys."""
-    known = {f.name for f in fields(ExperimentConfig)}
-    kwargs = {k: v for k, v in mapping.items() if k in known}
-    return cls(**kwargs)
+    @classmethod
+    def from_dict(cls, mapping: Mapping[str, Any]) -> ExperimentConfig:
+        """Build :class:`ExperimentConfig` from a YAML/dict, ignoring unknown keys."""
+        known = {f.name for f in fields(ExperimentConfig)}
+        kwargs = {k: v for k, v in mapping.items() if k in known}
+        return cls(**kwargs)
